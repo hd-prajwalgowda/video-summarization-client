d4accccb131b5ddc67ea4fd878f8edf7f4a888c5
Nidhi Talastha <talasthanidhi@gmail.com>
2021-05-30 18:16:30 +0530

    resolves merge conflict from auth-flow to main

---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Navbar = ({ title, onClick, icon }) => {
+  return (
+    <button  className="py-1 px-3 border-2 border-blue-500 rounded-sm text-blue-500">
+      Click
+    </button>
+  );
+};
+
+export default Navbar;
