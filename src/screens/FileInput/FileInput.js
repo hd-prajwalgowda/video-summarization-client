4f389e01867a2a04e1ad941d1395b3bb4d6d4668
hd-prajwalgowda <hdprajwalgowda@gmail.com>
2021-07-17 23:09:25 +0530

    Adds drag and drop functionality for file upload

--- src/screens/FileInput/FileInput.js
+++ src/screens/FileInput/FileInput.js
@@ -1,11 +1,46 @@
-import React, { useState } from 'react';
+import React, { useState,useCallback } from 'react';
 import axios from 'axios';
 import Layout from '../../components/Layout';
-
+import {useDropzone} from 'react-dropzone'
 const FileInput = () => {
   const [video, setVideo] = useState('');
   const [videoURL, setVideoURL] = useState('');
   const [fileName, setFileName] = useState('');
+
+
+  const onDrop = useCallback(acceptedFiles => {
+    // Do something with the files
+      const file = acceptedFiles[0]
+      const reader = new FileReader()
+
+      const formData = new FormData();
+
+      setFileName(file['name'])
+
+      formData.append('file', file);
+
+      const config = {
+        headers: {
+          'content-type': 'multipart/form-data',
+        },
+      };
+
+      axios.post('http://localhost:5000/api/upload_video', formData, config);
+      console.log(video,videoURL)
+
+
+      reader.onabort = () => console.log('file reading was aborted')
+      reader.onerror = () => console.log('file reading has failed')
+      reader.onload = () => {
+      // Do whatever you want with the file contents
+        const binaryStr = reader.result
+        console.log(binaryStr)
+      }
+      reader.readAsArrayBuffer(file)
+  }, [])
+  const {getRootProps, getInputProps, isDragActive} = useDropzone({onDrop})
+
+
 
   const getVideo = (e) => {
     e.preventDefault();
@@ -57,17 +92,20 @@ const FileInput = () => {
 
   return (
     <Layout>
-      <div>
-
-          <label id="dropContainer" className="p-96 border-blue-500" htmlFor="video-upload">
-            {fileName === '' ? 'Upload File' : fileName }
+      <div className="flex justify-center items-center">
+          <div id="dropContainer" {...getRootProps()} className="w-1/2 py-60 border-2 rounded border-dashed border-blue-500 flex justify-center items-center">
+            {fileName === '' ? <div className="flex flex-col items-center">
+              <div>Drag and drop file</div>
+              <div>or</div>
+              <div className="w-full justify-center py-2  px-3 flex items-center space-x-3 rounded border-2 border-blue-500 text-blue-500  hover:bg-blue-500 hover:text-white"> Upload File</div>
+            </div> : fileName }
             <input
+            {...getInputProps()}
               className="hidden"
-              id="video-upload"
-              type="file"
-              onChange={getVideo}
+              // type="file"
+              // onChange={(e)=>getVideo(e)}
             />
-          </label>
+          </div>
       </div>
     </Layout>
   );
